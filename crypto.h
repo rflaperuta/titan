746c03857aa423a4e2d9e179f5b6c947f6360fec
Niko Rosvall <niko@byteptr.com>
2017-01-24 16:27:28 +0200

    Change to use MIT license
    Signed-off-by: Niko Rosvall <niko@byteptr.com>

--- crypto.h
+++ crypto.h
@@ -1,21 +1,5 @@
 /*
  * Copyright (C) 2017 Niko Rosvall <niko@byteptr.com>
- *
- * This file is part of Titan.
- *
- * Titan is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Titan is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Titan. If not, see <http://www.gnu.org/licenses/>.
- *
  */
 
 #ifndef __CRYPTO_H
